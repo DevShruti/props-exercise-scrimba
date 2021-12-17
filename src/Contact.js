bdca589f847776850e55bf21b8d9dda5d28f2c11
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-16 20:31:04 -0600

    DESTRUCTURING

--- src/Contact.js
+++ src/Contact.js
@@ -18,3 +18,21 @@ export default function Contact(props) {
         </div>
     )
 }
+
+// DESTRUCTURED WAY
+// export default function Contact({img, name, phone, email}) {
+//     return (
+//         <div className="contact-card">
+//             <img src={img}/>
+//             <h3>{name}</h3>
+//             <div className="info-group">
+//                 <img src="./images/phone-icon.png" />
+//                 <p>{phone}</p>
+//             </div>
+//             <div className="info-group">
+//                 <img src="./images/mail-icon.png" />
+//                 <p>{email}</p>
+//             </div>
+//         </div>
+//     )
+// }
